7cbb6bf8355f00ec445024162117c21e1a04c109
Amanshukla712 <120775204+Amanshukla712@users.noreply.github.com>
2024-04-05 23:01:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const dotenv = require("dotenv");
-const cors = require("cors");
+// const cors = require("cors");
 
 dotenv.config()
 
@@ -17,7 +17,7 @@ const wishlistRouter = require("./routes/wishlist.router")
 const connectDB = require('./config/dbconfig')
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 
 app.use(express.json())
 connectDB()
